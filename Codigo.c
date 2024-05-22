70a0fef7a66dc67b85d115218336980d783307b8
Narcisa-1156 <paolapinzon2514@gmail.com>
2024-05-22 00:00:12 -0500

    Update Codigo.c

--- Codigo.c
+++ Codigo.c
@@ -1,21 +1,26 @@
 #include<stdio.h>
-int entero, i;
-char caracter;
-float flotante;
-int main(){
+
+int main() {
+    //Declaracion de variables
+    int entero;
+    char caracter;
+    float flotante, suma;
+    //Mensaje de vienvenida
     printf("Hola!! \nBienvenido a mi programa\n");
     printf("Puedes ingresar 3 variables con tres tipos de datos\n");
+    //Ingreso de variables
     printf("Ingresa un caracter:\n");
-    scanf("%c", &caracter);
-    printf("Ingresa un decimal:\n");
-    scanf("%f", &flotante);
-    printf("Ingresa un entero:\n");
-    scanf("%d", &entero);
-    printf("==================================================================\n");
-    printf("Los valores ingresados son los soguientes:\n");
-    printf("char = %c\n", caracter);
-    printf("float = %f\n", flotante);
-    printf("entero = %d\n", entero);
+    scanf(" %c", &caracter); 
 
+    printf("Ingresa un decimal:\n");
+    scanf(" %f", &flotante);
+
+    printf("Ingresa un entero:\n");
+    scanf(" %d", &entero);
+    //Realizacion de una operacion basica
+    printf("==================================================================\n");
+    printf("Los valores numericos ingresados permiten la siguiente suma:\n");
+    suma = flotante + entero;
+    printf("%f\n", suma);
     return 0;
 }
